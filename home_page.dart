c06e7b28a508cc649969c21d74781ca149d0c493
Yahya Can Evcimen <71712807+canevcimen@users.noreply.github.com>
2023-04-04 16:30:44 +0300

    Resim yenilendi

--- home_page.dart
+++ home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          Image.asset('assets/codelab.png'),
+          Image.asset('assets/yeni-kampus-gisi.jpg'),
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'İstanbul'),
